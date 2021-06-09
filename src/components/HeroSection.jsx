1b8baed26607e074309489b48123207bc3e40cb4
unknown <rlmichaud4@outlook.com>
2021-06-09 13:35:02 -0400

    Add basic hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,11 @@ import '../styles/HeroSection.css';
 function HeroSection() {
     return (
         <div id="hero">
-            <img alt="" src="./images/circle-cropped.png"  />
+            <div className="hero-text">
+                <h1>Hi, I'm Rachel!</h1>
+                <p>Aspiring to be a full stack web developer</p>
+            </div>
+            <div><img alt="" src="./images/circle-cropped.png"  /></div>
         </div>
     );
 }
